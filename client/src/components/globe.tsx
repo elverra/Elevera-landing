7d46b531de476c66a8bb8ab36a4e15c040a4280b
ifidoucsoft <ephraim.oladokun.doucsoft.tech>
2025-09-10 18:20:25 +0000

    Heros-Section

--- client/src/components/globe.tsx
+++ client/src/components/globe.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 const Globe: React.FC = () => {
   return (
     <>
@@ -15,17 +14,29 @@ const Globe: React.FC = () => {
           @keyframes twinkling-fast { 0%,100% { opacity:0.1; } 50% { opacity:1; } }
         `}
       </style>
-      <div className="flex items-center justify-center ">
+      <div className="relative w-full h-[100vh]">
+        {/* Div pour l'image de fond avec flou */}
         <div
-          className="relative w-[300px] h-[300px] rounded-full overflow-hidden shadow-[0_0_40px_rgba(255,255,255,0.3),-10px_0_16px_#c3f4ff_inset,30px_4px_50px_#000_inset,-48px_-4px_68px_#c3f4ff99_inset,500px_0_88px_#00000066_inset,300px_0_76px_#000000aa_inset]"
+          className="absolute inset-0"
           style={{
-            backgroundImage:
-              "url('https://pub-940ccf6255b54fa799a9b01050e6c227.r2.dev/globe.jpeg')",
+            backgroundImage: "url('/public/lovable-uploads/Poster.png')",
             backgroundSize: "cover",
-            backgroundPosition: "left",
-            animation: "earthRotate 30s linear infinite",
+            backgroundPosition: "center",
+            filter: "blur(5px)",
           }}
-        >
+        ></div>
+        {/* Conteneur pour le globe et les étoiles, sans flou */}
+        <div className="flex items-center justify-center relative h-full">
+          <div
+            className="relative w-[300px] h-[300px] rounded-full overflow-hidden shadow-[0_0_40px_rgba(255,255,255,0.3),-10px_0_16px_#c3f4ff_inset,30px_4px_50px_#000_inset,-48px_-4px_68px_#c3f4ff99_inset,500px_0_88px_#00000066_inset,300px_0_76px_#000000aa_inset]"
+            style={{
+              backgroundImage:
+                "url('https://pub-940ccf6255b54fa799a9b01050e6c227.r2.dev/globe.jpeg')",
+              backgroundSize: "cover",
+              backgroundPosition: "left",
+              animation: "earthRotate 30s linear infinite",
+            }}
+          ></div>
           {/* Stars */}
           <div
             className="absolute left-[-20px] w-1 h-1 bg-white rounded-full"
