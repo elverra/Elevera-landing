69751b635f16db1e1930a23e554bd2c1e68e8550
Ephraim D Oladokun <oladokunefi123@gmail.com>
2025-09-17 11:24:11 +0000

    chore: update Vercel config and change participation button text

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from "@/components/ui/sonner"
 import { TooltipProvider } from "@/components/ui/tooltip"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { HashRouter, Routes, Route } from "react-router-dom"
 import { AuthProvider } from "@/hooks/useAuth"
 import { LanguageProvider } from "@/contexts/LanguageContext"
 import routes from "./routes"
@@ -15,7 +15,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <LanguageProvider>
-          <BrowserRouter>
+          <HashRouter>
             <AuthProvider>
               <Routes>
                 {routes.map((route, index) => (
@@ -25,7 +25,7 @@ function App() {
               </Routes>
               <Toaster />
             </AuthProvider>
-          </BrowserRouter>
+          </HashRouter>
         </LanguageProvider>
       </TooltipProvider>
     </QueryClientProvider>
