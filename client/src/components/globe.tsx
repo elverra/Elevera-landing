c96997c2501a163d9b96b21726a321bb6dcc5fd0
Ephraim D Oladokun <oladokunefi123@gmail.com>
2025-09-17 13:20:08 +0000

    chore: update globe background image from Poster.png to 2.jpeg

--- client/src/components/globe.tsx
+++ client/src/components/globe.tsx
@@ -51,7 +51,7 @@ const Globe: React.FC = () => {
         <div
           className="absolute inset-0"
           style={{
-            backgroundImage: "url('/lovable-uploads/Poster.png')",
+            backgroundImage: "url('/lovable-uploads/2.jpeg')",
             backgroundSize: "cover",
             backgroundPosition: "center",
             filter: "blur(5px)",
